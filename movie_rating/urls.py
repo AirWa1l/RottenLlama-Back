a26bfae2ad2da532f97c64cceb9f4a7751cad2a7
flowxter <mafla7441@gmail.com>
2025-06-16 19:46:05 -0500

    duplicate folder configuration and PU test

--- movie_rating/urls.py
+++ movie_rating/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')), # Users app urls
+    path('api/', include('peliculas.urls')),
 ]
