23d6cd65d8118a843eabb71df88f901467d3bf09
michaelRS2002 <ramirez.michael@correounivalle.edu.co>
2025-06-30 07:31:36 -0500

    NEW URL HEALTH

--- movie_rating/urls.py
+++ movie_rating/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
 from peliculas.views import import_peliculas 
+from django.http import JsonResponse
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')), # Users app urls
+    
     path('api/auth/', include('users.urls')),  
     path('api/', include('peliculas.urls')),
     path('api/peliculas/import/', import_peliculas, name='import_peliculas'),
